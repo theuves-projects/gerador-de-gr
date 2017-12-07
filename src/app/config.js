265c55a5f6ede67ce1841250227d69712db742bd
Matheus Alves <theuves@outlook.com>
2017-12-07 14:31:14 -0300

    config.js: corrige erro de dígitação

--- src/app/config.js
+++ src/app/config.js
@@ -8,17 +8,17 @@
   function config($routeProvider) {
     $routeProvider
       .when("/", {
-        templateUrl: "app/constrollers/Adicionar/template.html",
+        templateUrl: "app/controllers/Adicionar/template.html",
         controller: "Adicionar",
         controllerAs: "adic"
       })
       .when("/imprimir", {
-        templateUrl: "app/constrollers/Imprimir/template.html",
+        templateUrl: "app/controllers/Imprimir/template.html",
         controller: "Imprimir",
         controllerAs: "vm"
       })
       .when("/configurar", {
-        templateUrl: "app/constrollers/Configurar/template.html",
+        templateUrl: "app/controllers/Configurar/template.html",
         controller: "Configurar",
         controllerAs: "vm"
       })
