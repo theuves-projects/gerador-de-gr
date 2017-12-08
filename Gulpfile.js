a2856072a1c35ca44f8d3be47ff8bfd19915286e
Matheus Alves <theuves@outlook.com>
2017-12-08 16:34:51 -0300

    adiciona gulp e todas as suas dependências

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,38 +1,94 @@
 "use strict";
 
-var gulp = require("gulp");
-//////////////////////////
+var gulp = require('gulp');
+///////////////////////////
 
-var angularTemplatecache = require("gulp-angular-templatecache");
-var htmlreplace = require("gulp-html-replace");
-var concat = require("gulp-concat");
-var angularFilesort = require("gulp-angular-filesort");
+var runSequence = require('run-sequence');
+//////////////////////////////////////////
 
-gulp.task("cache",  function () {
-  return gulp.src("app/**/*.html")
-    .pipe(angularTemplatecache("templates.js", {
+var gulpLoadPlugins = require('gulp-load-plugins');
+var $ = gulpLoadPlugins();
+
+/**
+ * injetar
+ * =======
+ */
+gulp.task("injetar", function () {
+  runSequence(
+    "injetar:css",
+    "injetar:js"
+  );
+});
+
+/**
+ * injetar:css
+ * -----------
+ */
+gulp.task("injetar:css", function () {
+  var diretorios = [
+    "src/index.css",
+    "src/app/**/*.css"
+  ];
+
+  var arquivos = gulp.src(diretorios);
+
+  return gulp.src("src/index.html")
+    .pipe($.inject(arquivos, {relative: true}))
+    .pipe(gulp.dest("src"));
+});
+
+/**
+ * injetar:js
+ * ----------
+ */
+gulp.task("injetar:js", function () {
+  var arquivos = gulp.src("src/app/**/*.js")
+    .pipe($.angularFilesort());
+
+  return gulp.src("src/index.html")
+    .pipe($.inject(arquivos, {relative: true}))
+    .pipe(gulp.dest("src"));
+});
+
+/**
+ * construir
+ * =========
+ */
+gulp.task("construir", function () {
+  runSequence(
+    "html-css-e-js",
+    "templates"
+  );
+});
+
+/**
+ * construir:templates
+ * -------------------
+ */
+gulp.task("construir:templates", function () {
+  var templates = gulp.src("src/app/**/*.html")
+    .pipe($.htmlmin({collapseWhitespace: true}))
+    .pipe($.angularTemplatecache({
       module: "app",
       root: "app/"
-    }))
+    }));
 
-    .pipe(gulp.dest("app"));
-});
+  var scriptConstruido = gulp.src("build/script.js");
 
-gulp.task("js", ["cache"], function () {
-  return gulp.src("app/**/*.js")
-    .pipe(angularFilesort())
-    .pipe(concat("build.js"))
+  return $.merge(scriptConstruido, templates)
+    .pipe($.concat("script.js"))
+    .pipe($.uglify())
     .pipe(gulp.dest("build"));
 });
 
-gulp.task("html", ['js'], function () {
-  return gulp.src("index.html")
-    .pipe(htmlreplace({
-      js: "build.js"
-    }))
+/**
+ * construir:html-css-e-js
+ * -----------------------
+ */
+gulp.task("construir:html-css-e-js", function () {
+  return gulp.src("src/index.html")
+    .pipe($.useref())
+    .pipe($.if("*.js", $.ngAnnotate()))
+    .pipe($.if("*.css", $.csso()))
     .pipe(gulp.dest("build"));
 });
-
-gulp.task("default", [
-  "html"
-]);
