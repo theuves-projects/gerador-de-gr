05a6754a2e33bde9e1d4df4c432c7e5f9b0157ba
Matheus Alves <theuves@outlook.com>
2017-12-15 20:47:30 -0300

    corrige erro 'run.js'
    isso não funciona como eu pensava
    
    os dados só são executados na inicialização
    
    o '$anchorScroll' não haverá se vir de outra 'view'

--- src/app/run.js
+++ src/app/run.js
@@ -7,31 +7,17 @@
 
   function run(
       $window
-    , $anchorScroll
-    , $location
     , Configuracoes
     , Destinatarios
-    , Tela
+    , Guia
     ) {
-
-      // O scroll funcionará somente na
-      // página de configurações.
-      if ($location.path() === "/configurar") {
-        $anchorScroll.yOffset = 5;
-
-        $anchorScroll();
-      }
-
       if (Configuracoes.naoTem("listaDeDestinatarios")) {
         Destinatarios.iniciar();
       }
 
       if (Configuracoes.naoTem("nomeDoUsuario")) {
-        var nome = Tela.perguntar("Atenção", "Informe seu nome:");
+        var nome = $window.prompt("Informe seu nome:");
 
-        // (Caso o usuário tenha cancelado,
-        // pois isso retornaria `null`)
-        // +-----------------------------vv
         nome = angular.uppercase(nome || "");
 
         Configuracoes.adicionar("nomeDoUsuario", nome);
