07bc334ce16a7f68993f60fa9bddfc2b95118e5b
Matheus Alves <theuves@outlook.com>
2017-12-16 01:10:11 -0300

    adiciona opção de preencher dados

---
+++ src/app/filters/preencher.js
@@ -0,0 +1,15 @@
+;(function () {
+  "use strict";
+
+  angular
+    .module("app")
+    .filter("preencher", preencher);
+
+  function preencher() {
+    return function (num, pad) {
+      return num
+        .toString()
+        .padStart(pad, "0");
+    };
+  }
+})();
