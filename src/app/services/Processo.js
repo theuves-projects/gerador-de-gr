fbb8550a82ce461d92c0ec78d588ba1d4bbe2d51
unknown <theuves@gmail.com>
2017-12-30 20:09:34 -0400

    melhora service 'Processo'

--- src/app/services/Processo.js
+++ src/app/services/Processo.js
@@ -9,55 +9,69 @@
     var proc = this;
     ////////////////
 
+    proc.ehAntigo = function (numero) {
+      var desformatado = proc.desformatar(numero);
+      return desformatado.length === 12 || desformatado.length === 13;
+    };
+
+    proc.ehSimplificado = function (numero) {
+      var desformatado = proc.desformatar(numero);
+      return desformatado.length === 15;
+    };
+
     proc.ehValido = function (numero) {
-      return /^(\d{12,13}|\d{15}|\d{20})$/.test(proc.limpar(numero));
+      var desformatado = proc.desformatar(numero);
+      return /^(\d{12,13}|\d{15}|\d{20})$/.test(desformatado);
     };
 
     proc.formatar = function (numero) {
-      if (proc.ehValido(numero)) {
-        var desformatado = proc.limpar(numero);
-        var ehAntigo = desformatado.length === 12 || desformatado.length === 13;
-        var ehSimplificado = desformatado.length === 15;
+      var nEhValido = !proc.ehValido(numero);
 
-        if (ehAntigo) return proc.formatarAntigo(desformatado);
-        if (ehSimplificado) return proc.formatarNumSimplificado(desformatado);
+      if (nEhValido) return numero;
 
-        return proc.formatarNovo(desformatado);
+      var ehAntigo = proc.ehAntigo(numero);
+      var ehSimplificado = proc.ehSimplificado(numero);
+
+      var desformatado = proc.desformatar(numero);
+
+      if (ehAntigo) return proc.formatarPorTipo(desformatado, "antigo");
+      if (ehSimplificado) return proc.formatarPorTipo(desformatado, "simplificado");
+
+      return proc.formatarPorTipo(desformatado, "novo");
+    };
+
+    proc.formatarPorTipo = function (numero, tipo) {
+      var regex;
+      var mascara;
+
+      switch (tipo) {
+        case "antigo":
+          regex = /^(\d{3,4})(\d{2})(\d{6})(\d{1})$/;
+          mascara = "$1.$2.$3-$4";
+          break;
+        case "novo":
+          regex = /^(\d{7})(\d{2})(\d{4})(\d{1})(\d{2})(\d{4})$/;
+          mascara = "$1-$2.$3.$4.$5.$6";
+          break;
+        case "simplificado":
+          regex = /^(\d{4})(\d{2})(\d{2})(\d{6})(\d)$/;
+          mascara = "$1.$2.$3.$4-$5";
+          break;
       }
 
-      return numero;
-    };
-
-    proc.formatarAntigo = function (numero) {
-      var regex = /^(\d{3,4})(\d{2})(\d{6})(\d{1})$/;
-      var mascara = "$1.$2.$3-$4";
-
       return numero.replace(regex, mascara);
     };
 
-    proc.formatarNovo = function (numero) {
-      var regex = /^(\d{7})(\d{2})(\d{4})(\d{1})(\d{2})(\d{4})$/;
-      var mascara = "$1-$2.$3.$4.$5.$6";
+    proc.desformatar = function (numero) {
+      var emString = numero.toString();
+      var aparado = emString.trim();
+      var desformatado = aparado.replace(/[+-.\s]/g, "");
 
-      return numero.replace(regex, mascara);
-    };
+      // nenhum número de processo desformatado tem mais de 20 caracteres.
+      var temDadosDesnecessarios = desformatado.length > 20;
 
-    proc.formatarNumSimplificado = function (numero) {
-      var re = /^(\d{4})(\d{2})(\d{2})(\d{6})(\d)$/;
-      var mask = "$1.$2.$3.$4-$5";
-
-      return numero.replace(re, mask);
-    };
-
-    proc.limpar = function (numero) {
-      var limpo = numero
-        .toString()
-        .trim()
-        .replace(/[+-.\s]/g, "");
-
-      return limpo.length > 20
-        ? limpo.replace(/\d{5}$/g, "")
-        : limpo;
+      if (temDadosDesnecessarios) return limpo.replace(/\d{5}$/g, "");
+      return desformatado;
     };
   }
 })(window.angular);
