1d263a749fd86dd138a4b5a6703be4fdc394cdb0
Matheus Alves <theuves@outlook.com>
2018-01-14 19:28:18 -0300

    melhora tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,114 +1,60 @@
 "use strict";
+const gulp = require('gulp');
+const merge = require("merge-stream");
+const concat = require("gulp-concat");
+const csso = require("gulp-csso");
+const htmlmin = require("gulp-htmlmin");
+const inject = require("gulp-inject");
+const inlineSource = require("gulp-inline-source");
+const ngAnnotate = require("gulp-ng-annotate");
+const ngFilesort = require("gulp-angular-filesort");
+const ngTemplate = require("gulp-angular-templatecache");
+const order = require("gulp-order");
+const uglify = require("gulp-uglify");
 
-var gulp = require('gulp');
-///////////////////////////
+gulp.task("build:js", () => {
+  const app = gulp.src("src/app/**/*.js")
+    .pipe(ngFilesort())
+    .pipe(ngAnnotate())
+    .pipe(concat("app.js"));
 
-var runSequence = require('run-sequence');
-var $ = require('gulp-load-plugins')();
+  const templates = gulp.src("src/app/**/*.html")
+    .pipe(inlineSource({attribute: false, rootpath: "src"}))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(ngTemplate({module: "app", root: "app/"}));
 
-/**
- * construir
- * =========
- */
-gulp.task("construir", function () {
-  runSequence(
-    "construir:html-css-e-js",
-    "construir:templates",
-    "mover"
-  );
-});
-
-/**
- * construir:templates
- * -------------------
- */
-gulp.task("construir:templates", function () {
-  var templates = gulp.src("src/app/**/*.html")
-    .pipe($.inlineSource({
-      attribute: false,
-      rootpath: "src"
-    }))
-    .pipe($.htmlmin({collapseWhitespace: true}))
-    .pipe($.angularTemplatecache({
-      module: "app",
-      root: "app/"
-    }));
-
-  var scriptJahConstruido = gulp.src("build/script.js");
-
-  return $.merge(scriptJahConstruido, templates)
-    .pipe($.concat("script.js"))
-    .pipe($.uglify())
+  merge(app, templates)
+    .pipe(order())
+    .pipe(concat("script.js"))
+    .pipe(uglify())
     .pipe(gulp.dest("build"));
 });
 
-/**
- * construir:html-css-e-js
- * -----------------------
- */
-gulp.task("construir:html-css-e-js", function () {
-  return gulp.src("src/index.html")
-    .pipe($.useref())
-    .pipe($.if("*.js", $.ngAnnotate()))
-    .pipe($.if("*.css", $.csso()))
+gulp.task("build:css", () => {
+  gulp.src("src/**/*.css")
+    .pipe(concat("style.css"))
+    .pipe(csso())
     .pipe(gulp.dest("build"));
 });
 
-/**
- * injetar
- * =======
- */
-gulp.task("injetar", function () {
-  runSequence(
-    "injetar:css",
-    "injetar:js"
-  );
-});
+gulp.task("build:inject", () => {
+  const sources = gulp.src("build/**/*.{js,css}");
+  const options = {ignorePath: "build", addRootSlash: false, removeTags: true};
 
-/**
- * injetar:css
- * -----------
- */
-gulp.task("injetar:css", function () {
-  var diretorios = [
-    "src/**/*.css"
-  ];
-
-  var arquivos = gulp.src(diretorios);
-
-  return gulp.src("src/index.html")
-    .pipe($.inject(arquivos, {relative: true}))
-    .pipe(gulp.dest("src"));
-});
-
-/**
- * injetar:js
- * ----------
- */
-gulp.task("injetar:js", function () {
-  var arquivos = gulp.src("src/app/**/*.js")
-    .pipe($.angularFilesort());
-
-  return gulp.src("src/index.html")
-    .pipe($.inject(arquivos, {relative: true}))
-    .pipe(gulp.dest("src"));
-});
-
-/**
- * mover
- * =====
- */
-gulp.task("mover", function () {
-  runSequence(
-    "mover:favicon.ico"
-  );
-});
-
-/**
- * mover:favicon.ico
- * -----------------
- */
-gulp.task("mover:favicon.ico", function () {
-  return gulp.src("src/favicon.ico")
+  gulp.src("src/index.html")
+    .pipe(inject(sources, options))
     .pipe(gulp.dest("build"));
+});
+
+gulp.task("build:copy", () => {
+  gulp.src("src/favicon.ico").pipe(gulp.dest("build"));
+});
+
+gulp.task("build", ["build:js", "build:css", "build:inject", "build:copy"]);
+
+gulp.task("watch", () => {
+  gulp.watch("src/**/*.css", ["build:css"]);
+  gulp.watch("src/app/**/*.{js,html}", ["build:js"]);
+  gulp.watch("src/favicon.ico", ["build:copy"]);
+  gulp.watch("src/index.html", ["build:inject"]);
 });
